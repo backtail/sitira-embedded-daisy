cdba5ae5efc53b7d1ba9071fe10ade6f2a9b6377
backtail <accounts@maxgenson.de>
2022-10-25 16:43:05 +0200

    adapt daisy for new sitira hardware

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,8 @@ pub mod rgbled;
 pub mod sd_card;
 pub mod sitira;
 
-pub const CONTROL_RATE_IN_MS: u32 = 10;
-pub const LCD_REFRESH_RATE_IN_MS: u32 = 50;
+pub const CONTROL_RATE_IN_MS: u32 = 20;
+pub const LCD_REFRESH_RATE_IN_MS: u32 = 20;
 pub const RECORD_SIZE: usize = 0x2000000;
 
 #[rtic::app(
@@ -17,11 +17,11 @@ pub const RECORD_SIZE: usize = 0x2000000;
 )]
 mod app {
     use crate::{
-        rgbled::RGBColors,
         sitira::{AudioRate, ControlRate, Sitira, VisualRate},
-        CONTROL_RATE_IN_MS, RECORD_SIZE,
+        CONTROL_RATE_IN_MS,
     };
     use granulator::{Granulator, GranulatorParameter::*};
+
     use libdaisy::prelude::*;
 
     #[cfg(feature = "log")]
@@ -30,10 +30,7 @@ mod app {
     #[shared]
     struct Shared {
         granulator: Granulator,
-        recording_state_switched: bool,
-        is_recording: bool,
         sdram: &'static mut [f32],
-        source_length: usize,
     }
 
     #[local]
@@ -41,8 +38,6 @@ mod app {
         ar: AudioRate,
         cr: ControlRate,
         vr: VisualRate,
-        parameter_page: usize,
-        shift: bool,
     }
 
     #[init]
@@ -70,17 +65,12 @@ mod app {
         (
             Shared {
                 granulator,
-                recording_state_switched: true,
-                is_recording: false,
                 sdram: sitira.sdram,
-                source_length: 0,
             },
             Local {
                 ar: sitira.audio_rate,
                 cr: sitira.control_rate,
                 vr: sitira.visual_rate,
-                parameter_page: 0,
-                shift: false,
             },
             init::Monotonics(),
         )
@@ -96,140 +86,49 @@ mod app {
     }
 
     // Interrupt handler for audio
-    #[task(binds = DMA1_STR1, local = [ar], shared = [granulator, sdram, is_recording, source_length], priority = 8)]
-    fn audio_handler(mut ctx: audio_handler::Context) {
+    #[task(binds = DMA1_STR1, local = [ar], shared = [granulator], priority = 8)]
+    fn audio_handler(ctx: audio_handler::Context) {
         let audio = &mut ctx.local.ar.audio;
         let mut buffer = ctx.local.ar.buffer;
 
         let mut granulator = ctx.shared.granulator;
-        let mut sdram = ctx.shared.sdram;
-        let mut is_recording = false;
-        ctx.shared.is_recording.lock(|f| is_recording = *f);
-        let mut source_length = 0;
-        ctx.shared.source_length.lock(|f| source_length = *f);
 
         audio.get_stereo(&mut buffer);
 
-        if is_recording && source_length < RECORD_SIZE {
-            sdram.lock(|sdram| {
-                for (index, (right, left)) in buffer.iter().enumerate() {
-                    sdram[source_length + index] = (right + left) * 0.5;
-                    audio.push_stereo((*right, *left)).unwrap();
-                }
-            });
-
-            ctx.shared
-                .source_length
-                .lock(|length| *length += buffer.len());
-        }
-
-        if !is_recording {
-            granulator.lock(|granulator| {
-                for _ in buffer {
-                    let mono_sample = granulator.get_next_sample();
-                    audio.push_stereo((mono_sample, mono_sample)).unwrap();
-                }
-            });
-        }
+        granulator.lock(|granulator| {
+            for _ in buffer {
+                let mono_sample = granulator.get_next_sample();
+                audio.push_stereo((mono_sample, mono_sample)).unwrap();
+            }
+        });
     }
 
     // read values from pot 2 and switch 2 of daisy pod
-    #[task(binds = TIM2, local = [cr, parameter_page, shift], shared = [granulator, recording_state_switched, is_recording])]
+    #[task(binds = TIM2, local = [cr], shared = [granulator])]
     fn update_handler(mut ctx: update_handler::Context) {
         // clear TIM2 interrupt flag
         ctx.local.cr.timer2.clear_irq();
 
-        // get all hardware
-        let adc1 = &mut ctx.local.cr.adc1;
-        let pot1 = &mut ctx.local.cr.pot1;
-        let pot2 = &mut ctx.local.cr.pot2;
-        let switch1 = &mut ctx.local.cr.switch1;
-        let switch2 = &mut ctx.local.cr.switch2;
+        let gate1 = &mut ctx.local.cr.gate1;
+        let gate2 = &mut ctx.local.cr.gate2;
+        let gate3 = &mut ctx.local.cr.gate3;
+        let gate4 = &mut ctx.local.cr.gate4;
         let led1 = &mut ctx.local.cr.led1;
         let led2 = &mut ctx.local.cr.led2;
-        let encoder = &mut ctx.local.cr.encoder;
 
-        // local parameters
-        let parameter_page = ctx.local.parameter_page;
-        let shift = ctx.local.shift;
-
-        // shared
-        let mut granulator = ctx.shared.granulator;
-        let recording_state_switched = &mut ctx.shared.recording_state_switched;
-        let is_recording = &mut ctx.shared.is_recording;
-
-        // update all the hardware
-        if let Ok(data) = adc1.read(pot1.get_pin()) {
-            pot1.update(data);
-        }
-        if let Ok(data) = adc1.read(pot2.get_pin()) {
-            pot2.update(data);
-        }
-        led1.update();
-        led2.update();
-        switch1.update();
-        switch2.update();
-        encoder.update();
-
-        // parameter pages
-        if switch2.is_held() {
-            *parameter_page += 1;
-            if *parameter_page > 4 {
-                *parameter_page = 0;
-            }
-        }
-
-        if *parameter_page == 0 {
-            led2.set_simple_color(RGBColors::Blue);
-            granulator.lock(|g| {
-                g.set_parameter(GrainSize, pot1.get_value());
-                g.set_parameter(GrainSizeSpread, pot2.get_value());
-            });
-        }
-        if *parameter_page == 1 {
-            led2.set_simple_color(RGBColors::Green);
-            granulator.lock(|g| {
-                g.set_parameter(Offset, pot1.get_value());
-                g.set_parameter(OffsetSpread, pot2.get_value());
-            });
-        }
-        if *parameter_page == 2 {
-            led2.set_simple_color(RGBColors::Magenta);
-            granulator.lock(|g| {
-                g.set_parameter(Pitch, pot1.get_value());
-                g.set_parameter(PitchSpread, pot2.get_value());
-            });
-        }
-        if *parameter_page == 3 {
-            led2.set_simple_color(RGBColors::Cyan);
-            granulator.lock(|g| {
-                g.set_parameter(Delay, pot1.get_value());
-                g.set_parameter(DelaySpread, pot2.get_value());
-            });
-        }
-        if *parameter_page == 4 {
-            led2.set_simple_color(RGBColors::White);
-            granulator.lock(|g| {
-                g.set_parameter(ActiveGrains, pot1.get_value());
-                g.set_parameter(MasterVolume, pot2.get_value());
-            });
-        }
-
-        // shift button
-        if switch1.is_held() {
-            *shift = !*shift;
-            recording_state_switched.lock(|f| *f = true);
+        if gate1.is_low().unwrap() || gate3.is_low().unwrap() {
+            led1.set_high().unwrap();
         } else {
-            recording_state_switched.lock(|f| *f = false);
+            led1.set_low().unwrap();
         }
 
-        if *shift {
-            led1.set_simple_color(RGBColors::Red);
-            is_recording.lock(|f| *f = true);
+        if gate2.is_low().unwrap() || gate4.is_low().unwrap() {
+            led2.set_high().unwrap();
         } else {
-            led1.set_simple_color(RGBColors::Black);
-            is_recording.lock(|f| *f = false);
+            led2.set_low().unwrap();
         }
+
+        let granulator = &mut ctx.shared.granulator;
 
         // update the scheduler
         granulator.lock(|g| {
@@ -237,72 +136,10 @@ mod app {
         });
     }
 
-    #[task(binds = TIM4, local = [vr], shared = [sdram, source_length, recording_state_switched, is_recording, granulator])]
-    fn display_handler(mut ctx: display_handler::Context) {
+    #[task(binds = TIM4, local = [vr], shared = [sdram])]
+    fn display_handler(ctx: display_handler::Context) {
         // clear TIM2 interrupt flag
         ctx.local.vr.timer4.clear_irq();
-
-        // shared
-        let mut recording_state_switched = false;
-        ctx.shared
-            .recording_state_switched
-            .lock(|f| recording_state_switched = *f);
-        let mut is_recording = false;
-        ctx.shared.is_recording.lock(|f| is_recording = *f);
-
-        let mut source_length = 0;
-        ctx.shared.source_length.lock(|f| source_length = *f);
-
-        let mut sdram = ctx.shared.sdram;
-
-        // setup
-        let lcd = &mut ctx.local.vr.lcd;
-
-        if recording_state_switched {
-            use embedded_graphics::geometry::Point;
-            use embedded_graphics::pixelcolor::Rgb565;
-            use embedded_graphics::prelude::*;
-            if is_recording {
-                sdram.lock(|sdram| sdram.fill(0.0));
-                ctx.shared.source_length.lock(|length| *length = 0);
-                lcd.fill_subsection_with_corners(
-                    Point { x: 0, y: 0 },
-                    Point { x: 61, y: 7 },
-                    Rgb565::BLACK,
-                );
-                lcd.fill_subsection_with_corners(
-                    Point { x: 62, y: 0 },
-                    Point { x: 82, y: 7 },
-                    Rgb565::CSS_RED,
-                );
-                lcd.print_on_screen(0, 5, "recording: on");
-            } else {
-                lcd.fill_subsection_with_corners(
-                    Point { x: 0, y: 0 },
-                    Point { x: 61, y: 7 },
-                    Rgb565::BLACK,
-                );
-                lcd.fill_subsection_with_corners(
-                    Point { x: 61, y: 0 },
-                    Point { x: 87, y: 7 },
-                    Rgb565::BLACK,
-                );
-                lcd.print_on_screen(0, 5, "recording: off");
-
-                sdram.lock(|sdram| {
-                    let audio_buffer = &sdram[0..source_length];
-                    lcd.fill_subsection_with_corners(
-                        Point { x: 0, y: 60 },
-                        Point { x: 320, y: 180 },
-                        Rgb565::BLACK,
-                    );
-                    lcd.draw_waveform(audio_buffer);
-                    ctx.shared.granulator.lock(|g| {
-                        g.set_audio_buffer(audio_buffer);
-                    });
-                });
-            }
-        }
 
         // activate timer 4 interrupt
         rtic::pend(stm32h7xx_hal::interrupt::TIM4);
--- src/sitira.rs
+++ src/sitira.rs
@@ -1,6 +1,7 @@
 use libdaisy::prelude::*;
 use libdaisy::{audio, gpio::*, hid, sdmmc, system::System};
 
+use stm32h7xx_hal::gpio::Floating;
 use stm32h7xx_hal::spi::{Enabled, Mode, Spi};
 use stm32h7xx_hal::stm32::SPI1;
 use stm32h7xx_hal::time::U32Ext;
@@ -9,27 +10,50 @@ use stm32h7xx_hal::{adc, pac, stm32};
 
 use crate::encoder;
 use crate::lcd;
-use crate::rgbled::*;
-use crate::sd_card::{self, SdCard};
+// use crate::sd_card::{self, SdCard};
 use crate::{CONTROL_RATE_IN_MS, LCD_REFRESH_RATE_IN_MS};
 
-pub type Pot1 = hid::AnalogControl<Daisy21<Analog>>;
-pub type Pot2 = hid::AnalogControl<Daisy15<Analog>>;
-pub type Led1 =
-    RGBLed<Daisy20<Output<PushPull>>, Daisy19<Output<PushPull>>, Daisy18<Output<PushPull>>>;
-pub type Led2 =
-    RGBLed<Daisy17<Output<PushPull>>, Daisy24<Output<PushPull>>, Daisy23<Output<PushPull>>>;
-pub type Switch1 = hid::Switch<Daisy27<Input<PullUp>>>;
-pub type Switch2 = hid::Switch<Daisy28<Input<PullUp>>>;
+// ===================
+// PIN TYPE DEFINITION
+// ===================
 
-pub type Encoder =
-    encoder::RotaryEncoder<Daisy13<Input<PullUp>>, Daisy25<Input<PullUp>>, Daisy26<Input<PullUp>>>;
+/// Not multiplexed
+pub type MasterVolume = hid::AnalogControl<Daisy21<Analog>>;
+/// MUX A+B
+pub type MuxInput1 = Daisy15<Input<Analog>>;
+/// MUX C+D
+pub type MuxInput2 = Daisy16<Input<Analog>>;
+
+pub type MuxSelect0 = Daisy17<Output<PushPull>>;
+pub type MuxSelect1 = Daisy18<Output<PushPull>>;
+pub type MuxSelect2 = Daisy19<Output<PushPull>>;
+
+pub type AnalogRead = f32;
+
+pub type Gate1 = Daisy24<Input<Floating>>;
+pub type Gate2 = Daisy25<Input<Floating>>;
+pub type Gate3 = Daisy22<Input<Floating>>;
+pub type Gate4 = Daisy23<Input<Floating>>;
+
+pub type KillGate = Daisy20<Input<Floating>>;
+
+pub type Led1 = Daisy13<Output<PushPull>>;
+pub type Led2 = Daisy14<Output<PushPull>>;
+pub type Led3 = Daisy0<Output<PushPull>>;
+
+pub type ButtonSwitch = hid::Switch<Daisy9<Input<Floating>>>;
+
+pub type Encoder = encoder::RotaryEncoder<
+    Daisy28<Input<Floating>>,
+    Daisy26<Input<PullUp>>,
+    Daisy27<Input<PullUp>>,
+>;
 
 pub type Display = lcd::Lcd<
     Spi<SPI1, Enabled>,
     Daisy11<Output<PushPull>>,
     Daisy12<Output<PushPull>>,
-    Daisy16<Output<PushPull>>,
+    Daisy7<Output<PushPull>>,
 >;
 
 pub struct AudioRate {
@@ -42,13 +66,25 @@ pub struct ControlRate {
     pub adc1: adc::Adc<stm32::ADC1, adc::Enabled>,
     pub timer2: Timer<stm32::TIM2>,
 
-    // Libdaisy
-    pub pot1: Pot1,
-    pub pot2: Pot2,
+    // Analog inputs
+    pub master_volume: MasterVolume,
+    pub muxed_parameters: AnalogRead,
+
+    // Gates
+    pub gate1: Gate1,
+    pub gate2: Gate2,
+    pub gate3: Gate3,
+    pub gate4: Gate4,
+    pub kill_gate: KillGate,
+
+    // LEDs
     pub led1: Led1,
     pub led2: Led2,
-    pub switch1: Switch1,
-    pub switch2: Switch2,
+    pub led3: Led3,
+    pub seed_led: SeedLed,
+
+    // Switches
+    pub button: ButtonSwitch,
     pub encoder: Encoder,
 }
 
@@ -62,11 +98,24 @@ pub struct Sitira {
     pub control_rate: ControlRate,
     pub visual_rate: VisualRate,
     pub sdram: &'static mut [f32],
-    pub sd_card: SdCard,
+    // pub sd_card: SdCard,
 }
 
 impl Sitira {
+    /**
+    Initializes the Daisy Seed for the Sitira platform. Automatically sets up all necessary peripherals:
+    - SAI1/I²C (I²S Audio Codec/Configuration)
+    - FMC (SDRAM Controller)
+    - TIM2/TIM3/TIM4 (Internal Timing for Interrupts)
+    - ADC1 (Analog Input Reading)
+    - SPI1 (LCD Driver)
+    - SDMMC1 (SD Card Controller)
+    */
     pub fn init(core: rtic::export::Peripherals, device: stm32::Peripherals) -> Self {
+        // ===========
+        // SYSTEM INIT
+        // ===========
+
         let mut system = System::init(core, device);
 
         let rcc_p = unsafe { pac::Peripherals::steal().RCC };
@@ -75,17 +124,24 @@ impl Sitira {
 
         let mut ccdr = System::init_clocks(pwr_p, rcc_p, &syscfg_p);
 
-        // set user led to low
+        // set high for system config
         let mut seed_led = system.gpio.led;
         seed_led.set_high().unwrap();
 
-        // setting up SDRAM
+        // ============
+        // CONFIG SDRAM
+        // ============
+
         let sdram = system.sdram;
         sdram.fill(0.0);
 
+        // =========================
+        // CONFIG SD CARD CONNECTION
+        // =========================
+
         // setting up SD card connection
         let sdmmc_d = unsafe { pac::Peripherals::steal().SDMMC1 };
-        let sd = sdmmc::init(
+        let _sd = sdmmc::init(
             system.gpio.daisy1.unwrap(),
             system.gpio.daisy2.unwrap(),
             system.gpio.daisy3.unwrap(),
@@ -97,32 +153,32 @@ impl Sitira {
             &mut ccdr.clocks,
         );
 
-        let sd_card = sd_card::SdCard::new(sd);
+        // let sd_card = sd_card::SdCard::new(sd);
 
-        // setup TIM2
+        // =============
+        // CONFIG TIMERS
+        // =============
 
         system.timer2.set_freq(CONTROL_RATE_IN_MS.ms());
 
-        // graphics
-
-        // setup hardware timer for LCD update rate
+        // Delay Timer
+        let timer3 = unsafe { pac::Peripherals::steal().TIM3 }.timer(
+            1.ms(),
+            ccdr.peripheral.TIM3,
+            &ccdr.clocks,
+        );
+        let delay = stm32h7xx_hal::delay::DelayFromCountDownTimer::new(timer3);
 
         let timer4_p = unsafe { pac::Peripherals::steal().TIM4 };
         let mut timer4 =
             stm32h7xx_hal::timer::Timer::tim4(timer4_p, ccdr.peripheral.TIM4, &mut ccdr.clocks);
 
-        timer4.set_freq(LCD_REFRESH_RATE_IN_MS.ms()); // 25Hz
+        timer4.set_freq(LCD_REFRESH_RATE_IN_MS.ms());
         timer4.listen(stm32h7xx_hal::timer::Event::TimeOut);
 
-        // setting up SPI1 for ILI9431 driver
-
-        let mut lcd_nss = system
-            .gpio
-            .daisy7
-            .expect("Failed to get pin 7 of the daisy!")
-            .into_push_pull_output();
-
-        lcd_nss.set_high().unwrap();
+        // ===========================
+        // CONFIG LCD DRIVER (ILI9431)
+        // ===========================
 
         let lcd_clk = system
             .gpio
@@ -150,10 +206,11 @@ impl Sitira {
             .expect("Failed to get pin 12 of the daisy!")
             .into_push_pull_output();
 
+        // dummy pin
         let lcd_reset = system
             .gpio
-            .daisy16
-            .expect("Failed to get pin 16 of the daisy!")
+            .daisy7
+            .expect("Failed to get pin 7 of the daisy!")
             .into_push_pull_output();
 
         let mode = Mode {
@@ -169,163 +226,172 @@ impl Sitira {
             &ccdr.clocks,
         );
 
-        let timer3 = unsafe { pac::Peripherals::steal().TIM3 }.timer(
-            1.ms(),
-            ccdr.peripheral.TIM3,
-            &ccdr.clocks,
-        );
-        let delay = stm32h7xx_hal::delay::DelayFromCountDownTimer::new(timer3);
-
         let mut lcd = lcd::Lcd::new(lcd_spi, lcd_dc, lcd_cs, lcd_reset, delay);
 
         lcd.setup();
 
-        // Setup ADC1
+        // =====================
+        // CONFIG ANALOG READING
+        // =====================
 
         let mut adc1 = system.adc1.enable();
         adc1.set_resolution(adc::Resolution::SIXTEENBIT);
-        let adc1_max_value = adc1.max_sample() as f32;
+        let _adc1_max_value = adc1.max_sample() as f32;
 
-        // setup analog reads from potentiometer
+        let mut adc2 = system.adc2.enable();
+        adc2.set_resolution(adc::Resolution::SIXTEENBIT);
+        let adc2_max_value = adc2.max_sample() as f32;
 
-        let pot1_pin = system
+        let master_volume_pin = system
             .gpio
             .daisy21
             .take()
-            .expect("Failed to get pin 21 of the daisy!")
+            .expect("Failed to get pin 13 of the daisy!")
             .into_analog();
+        let master_volume = hid::AnalogControl::new(master_volume_pin, adc2_max_value);
 
-        let pot1 = hid::AnalogControl::new(pot1_pin, adc1_max_value);
+        // ==============
+        // CONFIG ENCODER
+        // ==============
 
-        let pot2_pin = system
-            .gpio
-            .daisy15
-            .take()
-            .expect("Failed to get pin 15 of the daisy!")
-            .into_analog();
-
-        let pot2 = hid::AnalogControl::new(pot2_pin, adc1_max_value);
-
-        // setting up tactil switches
-
-        let switch1_pin = system
-            .gpio
-            .daisy27
-            .take()
-            .expect("Failed to get pin 27 of the daisy!")
-            .into_pull_up_input();
-        let mut switch1 = hid::Switch::new(switch1_pin, hid::SwitchType::PullUp);
-        switch1.set_held_thresh(Some(2));
-
-        let switch2_pin = system
+        let rotary_switch_pin = system
             .gpio
             .daisy28
             .take()
             .expect("Failed to get pin 28 of the daisy!")
-            .into_pull_up_input();
-        let mut switch2 = hid::Switch::new(switch2_pin, hid::SwitchType::PullUp);
-        switch2.set_held_thresh(Some(2));
-
-        // setup LEDs
-
-        let led1_red = system
-            .gpio
-            .daisy20
-            .take()
-            .expect("Failed to get pin 20 of the daisy!")
-            .into_push_pull_output();
-
-        let led1_green = system
-            .gpio
-            .daisy19
-            .take()
-            .expect("Failed to get pin 19 of the daisy!")
-            .into_push_pull_output();
-
-        let led1_blue = system
-            .gpio
-            .daisy18
-            .take()
-            .expect("Failed to get pin 18 of the daisy!")
-            .into_push_pull_output();
-
-        let led1 = RGBLed::new(led1_red, led1_green, led1_blue, LEDConfig::ActiveLow, 1000);
-
-        let led2_red = system
-            .gpio
-            .daisy17
-            .take()
-            .expect("Failed to get pin 17 of the daisy!")
-            .into_push_pull_output();
-
-        let led2_green = system
-            .gpio
-            .daisy24
-            .take()
-            .expect("Failed to get pin 24 of the daisy!")
-            .into_push_pull_output();
-
-        let led2_blue = system
-            .gpio
-            .daisy23
-            .take()
-            .expect("Failed to get pin 23 of the daisy!")
-            .into_push_pull_output();
-
-        let led2 = RGBLed::new(led2_red, led2_green, led2_blue, LEDConfig::ActiveLow, 1000);
-
-        // setting up rotary encoder
-
-        let rotary_switch_pin = system
-            .gpio
-            .daisy13
-            .take()
-            .expect("Failed to get pin 13 of the daisy!")
-            .into_pull_up_input();
+            .into_floating_input();
 
         let rotary_clock_pin = system
-            .gpio
-            .daisy25
-            .take()
-            .expect("Failed to get pin 25 of the daisy!")
-            .into_pull_up_input();
-
-        let rotary_data_pin = system
             .gpio
             .daisy26
             .take()
             .expect("Failed to get pin 26 of the daisy!")
             .into_pull_up_input();
 
+        let rotary_data_pin = system
+            .gpio
+            .daisy27
+            .take()
+            .expect("Failed to get pin 27 of the daisy!")
+            .into_pull_up_input();
+
         let mut encoder =
             encoder::RotaryEncoder::new(rotary_switch_pin, rotary_clock_pin, rotary_data_pin);
         encoder.switch.set_held_thresh(Some(2));
 
-        // audio stuff
+        // ==================
+        // CONFIG GATE INPUTS
+        // ==================
 
-        let buffer = [(0.0, 0.0); audio::BLOCK_SIZE_MAX]; // audio ring buffer
+        let gate1 = system
+            .gpio
+            .daisy24
+            .take()
+            .expect("Failed to get pin 24 of the daisy!")
+            .into_floating_input();
+
+        let gate2 = system
+            .gpio
+            .daisy25
+            .take()
+            .expect("Failed to get pin 25 of the daisy!")
+            .into_floating_input();
+
+        let gate3 = system
+            .gpio
+            .daisy22
+            .take()
+            .expect("Failed to get pin 22 of the daisy!")
+            .into_floating_input();
+
+        let gate4 = system
+            .gpio
+            .daisy23
+            .take()
+            .expect("Failed to get pin 23 of the daisy!")
+            .into_floating_input();
+
+        let kill_gate = system
+            .gpio
+            .daisy20
+            .take()
+            .expect("Failed to get pin 20 of the daisy!")
+            .into_floating_input();
+
+        // ===========
+        // CONFIG LEDs
+        // ===========
+
+        let mut led1 = system
+            .gpio
+            .daisy13
+            .take()
+            .expect("Failed to get pin 13 of the daisy!")
+            .into_push_pull_output();
+        led1.set_low().unwrap();
+
+        let mut led2 = system
+            .gpio
+            .daisy14
+            .take()
+            .expect("Failed to get pin 14 of the daisy!")
+            .into_push_pull_output();
+        led2.set_low().unwrap();
+
+        let mut led3 = system
+            .gpio
+            .daisy0
+            .take()
+            .expect("Failed to get pin 0 of the daisy!")
+            .into_push_pull_output();
+        led3.set_low().unwrap();
+
+        // =============
+        // CONFIG BUTTON
+        // =============
+
+        let button_pin = system
+            .gpio
+            .daisy9
+            .take()
+            .expect("Failed to get pin 9 of the daisy!")
+            .into_floating_input();
+
+        let button = hid::Switch::new(button_pin, hid::SwitchType::PullUp);
+        // button.set_held_thresh(Some(2));
+        // button.set_double_thresh(Some(5));
+
+        // ===============
+        // CONFIG FINISHED
+        // ===============
 
         seed_led.set_low().unwrap();
 
         Self {
             audio_rate: AudioRate {
                 audio: system.audio,
-                buffer,
+                buffer: [(0.0, 0.0); audio::BLOCK_SIZE_MAX],
             },
             control_rate: ControlRate {
                 adc1,
                 timer2: system.timer2,
-                pot1,
-                pot2,
+                master_volume,
+                muxed_parameters: 0.0, // placeholder
+                gate1,
+                gate2,
+                gate3,
+                gate4,
+                kill_gate,
                 led1,
                 led2,
-                switch1,
-                switch2,
+                led3,
+                seed_led,
+                button,
                 encoder,
             },
             visual_rate: VisualRate { lcd, timer4 },
             sdram,
-            sd_card,
+            // sd_card,
         }
     }
 }
